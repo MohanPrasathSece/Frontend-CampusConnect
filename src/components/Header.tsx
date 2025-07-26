2b3fc1a6f755063eddd6a5f53061d589816db838
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2025-07-26 15:33:29 +0530

    final

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,11 +25,7 @@ const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   
   useEffect(() => {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
+    document.documentElement.classList.remove('dark');
   }, []);
   const location = useLocation();
 
